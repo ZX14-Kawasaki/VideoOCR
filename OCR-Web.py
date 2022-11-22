44ca37ff2ebec3022402ebf54842da865125e949
ZX14-Kawasaki <tomaj003@hawaii.rr.com>
2022-11-21 16:45:55 -1000

    Update OCR-Web.py

--- OCR-Web.py
+++ OCR-Web.py
@@ -3,8 +3,6 @@ import streamlit as st  # Web App
 from PIL import Image  # Image Processing
 import numpy as np  # Image Processing
 
-#Compile and Open Terminal at bottom run in Chrome
-
 st.title("Kanji Optical Character Recognition")
 
 image = st.file_uploader(label="Upload your Kanji Image file", type=['png', 'jpg', 'jpeg'])
@@ -18,13 +16,14 @@ reader = load_model()  # load model
 
 if image is not None:
     input_image = Image.open(image)  # read image
-    st.image(input_image)  # display image
 
     with st.spinner("Processing ..."):
         result = reader.readtext(np.array(input_image))
         st.success("Results")
         for (bbox, text, prob) in result:
             st.write(text)
+            
+    st.image(input_image)  # display image
 
     st.success("Done!")
 else:
