c049c9e600cf9f4b266aadb5ffb10d452dc412c2
ZX14-Kawasaki <tomaj003@hawaii.rr.com>
2022-11-23 12:17:07 -1000

    Update OCR-Web.py

--- OCR-Web.py
+++ OCR-Web.py
@@ -2,6 +2,7 @@ import easyocr as ocr  # OCR
 import streamlit as st  # Web App
 from PIL import Image  # Image Processing
 import numpy as np  # Image Processing
+import cv2
 
 st.subheader("OCR - Optical Character Recognition")
 st.caption("Created by the Okinawan Genealogical Society of Hawaii")
@@ -26,9 +27,10 @@ reader = load_model()
 
 if image is not None:
     input_image = Image.open(image)  # read image
+    input_image = np.array(input_image)
 
     with st.spinner("Processing ..."):
-        result = reader.readtext(np.array(input_image))
+        result = reader.readtext(input_image)
         cnt = 0
         tot = 0
         for (bbox, text, prob) in result:
@@ -37,13 +39,21 @@ if image is not None:
         tot = tot / cnt   
         
         if (tot >= .75):
-            st.success("Results",icon="👍")
+            st.success("Results", icon="👍")
         else:
-            st.success("Results",icon="👎")
-            
+            st.success("Results", icon="👎")
+        cnt = 0
         for (bbox, text, prob) in result:
-            st.write(text)
-            
-    st.success("Image File",icon="👇")
-    st.image(input_image)           # display image
+            cnt = cnt + 1
+            st.write(cnt, text)
+            (tl, tr, br, bl) = bbox
+            tl = (int(tl[0]), int(tl[1]))
+            tr = (int(tr[0]), int(tr[1]))
+            br = (int(br[0]), int(br[1]))
+            bl = (int(bl[0]), int(bl[1]))
 
+            input_image = cv2.rectangle(input_image, tl, br, (0, 0, 0), 2)
+            input_image = cv2.putText(input_image, str(cnt), tr, cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 2, cv2.LINE_AA)
+    st.success("Image File", icon="👇")
+
+    st.image(input_image)           # display image
