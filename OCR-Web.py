b23139cefbdc6ace9ddf415ff96b4b6abbc5a8ad
ZX14-Kawasaki <tomaj003@hawaii.rr.com>
2022-11-27 13:04:16 -1000

    Update OCR-Web.py

--- OCR-Web.py
+++ OCR-Web.py
@@ -32,7 +32,7 @@ reader = load_model()
 if image is not None:
     input_image = Image.open(image)  # read image
     input_image = np.array(input_image)
-    sidebar.image(input_image)           # display image
+    st.sidebar.image(input_image)           # display image
     
     #dimensions = input_image.shape
     #st.write(dimensions)
