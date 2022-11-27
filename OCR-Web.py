bb9aa0dd2d50119d458427383bc4e9d4314d38c5
ZX14-Kawasaki <tomaj003@hawaii.rr.com>
2022-11-27 13:01:33 -1000

    Update OCR-Web.py

--- OCR-Web.py
+++ OCR-Web.py
@@ -53,7 +53,7 @@ if image is not None:
         cnt = 0
         for (bbox, text, prob) in result:
             cnt = cnt + 1
-            col1.write(cnt, text)
+            col1.write(text)
             (tl, tr, br, bl) = bbox
             tl = (int(tl[0]), int(tl[1]))
             tr = (int(tr[0]), int(tr[1]))
