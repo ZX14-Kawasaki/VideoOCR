e9e20f6c34f8263ec73f389be15a8b5d05801587
ZX14-Kawasaki <tomaj003@hawaii.rr.com>
2022-11-28 11:47:15 -1000

    Update OCR-Web.py

--- OCR-Web.py
+++ OCR-Web.py
@@ -71,15 +71,3 @@ if image is not None:
 
     col2.image(input_image)           # display image
     #st.snow()
-import pandas as pd
-import numpy as np
-import altair as alt
-
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-c = alt.Chart(chart_data).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-
-st.altair_chart(c, use_container_width=True)
