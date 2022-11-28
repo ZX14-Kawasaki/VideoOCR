ce7965440f87eaa2733c2849aae60a9c80fafd8b
ZX14-Kawasaki <tomaj003@hawaii.rr.com>
2022-11-28 11:49:12 -1000

    Update OCR-Web.py

--- OCR-Web.py
+++ OCR-Web.py
@@ -71,3 +71,16 @@ if image is not None:
 
     col2.image(input_image)           # display image
     #st.snow()
+from bokeh.plotting import figure
+
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+p = figure(
+    title='simple line example',
+    x_axis_label='x',
+    y_axis_label='y')
+
+p.line(x, y, legend_label='Trend', line_width=2)
+
+st.bokeh_chart(p, use_container_width=True)
